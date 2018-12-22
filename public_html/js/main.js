24296f49d75be73d65fae2c2771885112ce9bf5e
amanda <amanda.natallie.2@gmail.com>
2018-12-21 23:36:53 -0200

    Correções gerais....

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,9 +1,8 @@
-
 $(function () {
     $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top + 90
+            scrollTop: $($anchor.attr('href')).offset().top - 90
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
